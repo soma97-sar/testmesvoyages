24afb51695d7701dcb277d98b1ca5bb2616cf9af
BEN BAHA <BEN BAHA@sokal.lan>
2023-01-02 05:07:41 +0100

    Phase2: premier test" Hello world!"

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController {
+    /**
+     * @Route("/",name="accueil")
+     * @return Response
+     */
+    public function index(): Response {
+        return new Response('Hello world!');
+        
+    }
+    
+}
