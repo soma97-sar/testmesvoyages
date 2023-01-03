42f98aac79dc2eef5158f564f5fee5e799608636
BEN BAHA <BEN BAHA@sokal.lan>
2023-01-03 13:33:29 +0100

    Phase10: ORM(filtrage des enregistrements)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -11,6 +11,7 @@ use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 
 
@@ -45,6 +46,20 @@ class VoyagesController extends AbstractController {
         
     }
     /**
+     * @Route("/voyages/recherche/{champ}", name="voyages.findallequal")
+     * @param type $champ
+     * @param Request $request
+     * @return Response
+     */
+    public function findAllEqual($champ, Request $request):Response{
+        $valeur=$request->get("recherche");
+        $visites=$this->repository->findByEqualValue($champ, $valeur);
+        return $this->render("pages/voyages.html.twig",[
+            'visites'=>$visites
+        ]);
+    
+    }
+    /**
      * 
      * @var VisiteRepository
      */
@@ -54,7 +69,8 @@ class VoyagesController extends AbstractController {
      * @param VisiteRepository $repository
      */
     public function __construct(VisiteRepository $repository) {
-        $this->repository= $repository;
-        
-    }
+        $this->repository= $repository;    
+    }    
+    
 }
+
